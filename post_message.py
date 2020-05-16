100185eabf893499d1d528b1e7e00ee0145cf9dd
ownweight <kanamorishuhei@gmail.com>
2020-05-17 03:37:29 +0900

    lambdaがtmp配下のファイルしか書き込めないことに関して対応

--- post_message.py
+++ post_message.py
@@ -1,4 +1,4 @@
-import os, json, random, requests, datetime, csv
+import os, json, random, requests, datetime, csv, shutil
 
 WEBHOOK_URL = os.environ['WEBHOOK_URL']
 DEVELOPMENT_CHANNEL_URL = os.environ['DEVELOPMENT_CHANNEL_URL']
@@ -6,6 +6,10 @@ PRODUCTION_CHANNEL_URL = os.environ['PRODUCTION_CHANNEL_URL']
 ENVIRONMENT = os.environ['ENVIRONMENT']
 
 def post_message():
+
+    if not os.path.isfile("/tmp/rinban.csv"):
+        shutil.copyfile("./rinban.csv", "/tmp/rinban.csv")
+
     sentence = make_sentence(assign())
     requests.post(make_url(), data=json.dumps({ "text": sentence }))
     next_assign()
@@ -15,13 +19,13 @@ def assign():
         return json.load(f)[assign_number()]
 
 def assign_number():
-    lastrow = sum(1 for i in open('rinban.csv'))
-    with open('rinban.csv') as f:
+    lastrow = sum(1 for i in open('/tmp/rinban.csv'))
+    with open('/tmp/rinban.csv') as f:
         l = [row for row in csv.reader(f)]
         return l[lastrow - 1][0]
 
 def next_assign():
-    with open('rinban.csv', 'a', newline="") as f:
+    with open('/tmp/rinban.csv', 'a', newline="") as f:
        csv.writer(f).writerow([get_next_assign_number()])
 
 def get_next_assign_number():
